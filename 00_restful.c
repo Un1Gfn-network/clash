05673a72642f772d7f8b52cab1d9400983335689
Darren Ng <darren19970810@gmail.com>
2020-08-13 16:06:38 +0800

    get server name

--- 00_restful.c
+++ 00_restful.c
@@ -25,9 +25,23 @@ size_t write_callback(char *ptr, size_t size, size_t nmemb, void *userdata){
 
 }
 
-/*void assert_field(const json_object *const j,const char *const k,const char *const v){
-  
-}*/
+void assert_field(const json_object *const j,const char *const k,const char *const v){
+  json_object *p=NULL;
+  assert(json_object_object_get_ex(j,k,&p));
+  assert(p);
+  assert(json_type_string==json_object_get_type(p));
+  assert(0==strcmp(v,json_object_get_string(p)));
+  // assert(1==json_object_put(p));
+  // p=NULL;
+}
+
+const char *get_field(const json_object *const j,const char *const k){
+  json_object *p=NULL;
+  assert(json_object_object_get_ex(j,k,&p));
+  assert(p);
+  assert(json_type_string==json_object_get_type(p));
+  return json_object_get_string(p);
+}
 
 int main(){
 
@@ -41,6 +55,8 @@ int main(){
   assert(CURLE_OK==curl_easy_setopt(curl,CURLOPT_HEADER,0L));
   // assert(CURLE_OK==curl_easy_setopt(curl,CURLOPT_URL,"http://127.0.0.1:6170/proxies"));
   assert(CURLE_OK==curl_easy_setopt(curl,CURLOPT_URL,"http://127.0.0.1:6170/proxies/GLOBAL"));
+  // assert(CURLE_OK==curl_easy_setopt(curl,CURLOPT_URL,"http://127.0.0.1:6170/proxies/JP"));
+  // assert(CURLE_OK==curl_easy_setopt(curl,CURLOPT_URL,"http://127.0.0.1:6170/proxies/香港标准 IEPL 中继 19"));
   assert(CURLE_OK==curl_easy_perform(curl));
   // printf("%.*s\n",(int)sz,buf);
   assert(NULL!=(buf=realloc(buf,++sz)));
@@ -56,15 +72,10 @@ int main(){
   json_tokener_free(tok);
   tok=NULL;
 
-  json_object *p=NULL;
-  assert(json_object_object_get_ex(jobj,"name",&p));
-  assert(p);
-  assert(json_type_string==json_object_get_type(p));
-  assert(0==strcmp("GLOBAL",json_object_get_string(p)));
-  // assert(1==json_object_put(p));
-  p=NULL;
+  assert_field(jobj,"name","GLOBAL");
+  assert_field(jobj,"type","Selector");
+  printf("%s\n",get_field(jobj,"now"));
 
-  // printf("%s\n",json_object_to_json_string_ext(jobj,JSON_C_TO_STRING_PLAIN));
   // printf("%s\n",json_object_to_json_string_ext(jobj,JSON_C_TO_STRING_PLAIN|JSON_C_TO_STRING_SPACED));
   // printf("%s\n",json_object_to_json_string_ext(jobj,JSON_C_TO_STRING_PRETTY));
 
