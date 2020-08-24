a6cb2d58c15ff420b4e2cbc1105d2289980f1d74
Darren Ng <darren19970810@gmail.com>
2020-08-24 15:18:26 +0800

    clean up

--- test.c
+++ test.c
@@ -9,19 +9,17 @@
 #include <sys/socket.h>
 #include <linux/rtnetlink.h>
 
-// buffer to hold the RTNETLINK request
-struct {
-  struct nlmsghdr nl;
-  struct rtmsg    rt;
-  char            buf[8192];
-} req;
+// struct nlmsghdr nl={.nlmsg_len = NLMSG_LENGTH(sizeof(struct rtmsg))};
+struct nlmsghdr nl={};
+struct rtmsg rt={};
+char buf[8192]={};
 
 // variables used for
 // socket communications
-int fd;
-struct sockaddr_nl la;
-struct sockaddr_nl pa;
-struct msghdr msg;
+int fd=-1;
+struct sockaddr_nl la={};
+struct sockaddr_nl pa={.nl_family = AF_NETLINK};
+struct msghdr msg={};
 struct iovec iov;
 int rtn;
 
@@ -30,180 +28,116 @@ char buf[8192];
 
 // RTNETLINK message pointers & lengths
 // used when processing messages
-struct nlmsghdr *nlp;
-int nll;
-struct rtmsg *rtp;
-int rtl;
-struct rtattr *rtap;
+struct nlmsghdr *nlp=NULL;
+int nll=0;
+struct rtmsg *rtp=NULL;
+int rtl=0;
+struct rtattr *rtap=NULL;
 
-void form_request();
-void send_request();
-void recv_reply();
-void read_reply();
+int main(){
 
-int main()
-{
-
-  // open socket
+  // Open
   fd = socket(AF_NETLINK, SOCK_RAW, NETLINK_ROUTE);
+  assert(fd==3);
 
-  // setup local address & bind using
-  // this address
-  bzero(&la, sizeof(la));
-  la.nl_family = AF_NETLINK;
-  la.nl_pid = getpid();
+  // Bind
+  la=(struct sockaddr_nl){
+    .nl_family = AF_NETLINK,
+    .nl_pid = getpid()
+  };
   bind(fd, (struct sockaddr*) &la, sizeof(la));
 
+  // Send
+  nl=(struct nlmsghdr){
+    .nlmsg_len = NLMSG_LENGTH(sizeof(struct rtmsg)),
+    .nlmsg_flags = NLM_F_REQUEST | NLM_F_DUMP,
+    .nlmsg_type = RTM_GETROUTE
+  };
+  rt=(struct rtmsg){
+    .rtm_family = AF_INET,
+    .rtm_table = RT_TABLE_MAIN
+  };
+  iov=(struct iovec){
+    .iov_base = &nl,
+    .iov_len = nl.nlmsg_len
+  };
+  msg=(struct msghdr){
+    .msg_name = (void *) &pa,
+    .msg_namelen = sizeof(pa),
+    .msg_iov = &iov,
+    .msg_iovlen = 1
+  };
+  assert(1<=sendmsg(fd, &msg, 0));
 
-  // sub functions to create RTNETLINK message,
-  // send over socket, receive reply & process
-  // message
-  form_request();
-  send_request();
-  recv_reply();
-  read_reply();
-
-  // close socket
-  close(fd);
-}
-
-void send_request()
-{
-  // create the remote address
-  // to communicate
-  bzero(&pa, sizeof(pa));
-  pa.nl_family = AF_NETLINK;
-
-  // initialize & create the struct msghdr supplied
-  // to the sendmsg() function
-  bzero(&msg, sizeof(msg));
-  msg.msg_name = (void *) &pa;
-  msg.msg_namelen = sizeof(pa);
-
-  // place the pointer & size of the RTNETLINK
-  // message in the struct msghdr
-  iov.iov_base = (void *) &req.nl;
-  iov.iov_len = req.nl.nlmsg_len;
-  msg.msg_iov = &iov;
-  msg.msg_iovlen = 1;
-
-  // send the RTNETLINK message to kernel
-  rtn = sendmsg(fd, &msg, 0);
-}
-
-void recv_reply()
-{
-  char *p;
-
-  // initialize the socket read buffer
-  bzero(buf, sizeof(buf));
-
-  p = buf;
-  nll = 0;
-
+  // Receive
+  char *p = buf;
   // read from the socket until the NLMSG_DONE is
   // returned in the type of the RTNETLINK message
   // or if it was a monitoring socket
   while(1) {
     rtn = recv(fd, p, sizeof(buf) - nll, 0);
-
     nlp = (struct nlmsghdr *) p;
-
     if(nlp->nlmsg_type == NLMSG_DONE)
       break;
-
     // increment the buffer pointer to place
     // next message
     p += rtn;
-
     // increment the total size by the size of
     // the last received message
     nll += rtn;
-
-    if((la.nl_groups & RTMGRP_IPV4_ROUTE)
-                      == RTMGRP_IPV4_ROUTE)
+    if((la.nl_groups & RTMGRP_IPV4_ROUTE)== RTMGRP_IPV4_ROUTE)
       break;
   }
-}
 
-void form_request()
-{
-  // initialize the request buffer
-  bzero(&req, sizeof(req));
-
-  // set the NETLINK header
-  req.nl.nlmsg_len
-             = NLMSG_LENGTH(sizeof(struct rtmsg));
-  req.nl.nlmsg_flags = NLM_F_REQUEST | NLM_F_DUMP;
-  req.nl.nlmsg_type = RTM_GETROUTE;
-
-  // set the routing message header
-  req.rt.rtm_family = AF_INET;
-  req.rt.rtm_table = RT_TABLE_MAIN;
-}
-
-void read_reply()
-{
-  // string to hold content of the route
-  // table (i.e. one entry)
-  char dsts[24], gws[24], ifs[16], ms[24];
-
+  // Print
   // outer loop: loops thru all the NETLINK
   // headers that also include the route entry
   // header
   nlp = (struct nlmsghdr *) buf;
-  for(;NLMSG_OK(nlp, nll);nlp=NLMSG_NEXT(nlp, nll))
-  {
-
+  for(;NLMSG_OK(nlp, nll);nlp=NLMSG_NEXT(nlp, nll)){
     // get route entry header
     rtp = (struct rtmsg *) NLMSG_DATA(nlp);
-
     // we are only concerned about the
     // main route table
     if(rtp->rtm_table != RT_TABLE_MAIN)
       continue;
-
-    // init all the strings
-    bzero(dsts, sizeof(dsts));
-    bzero(gws, sizeof(gws));
-    bzero(ifs, sizeof(ifs));
-    bzero(ms, sizeof(ms));
-
+    // string to hold content of the route
+    // table (i.e. one entry)
+    char dsts[24]={}, gws[24]={}, ifs[16]={}, ms[24]={};
     // inner loop: loop thru all the attributes of
     // one route entry
     rtap = (struct rtattr *) RTM_RTA(rtp);
     rtl = RTM_PAYLOAD(nlp);
-    for(;RTA_OK(rtap, rtl);rtap=RTA_NEXT(rtap,rtl))
-    {
-      switch(rtap->rta_type)
-      {
+    for(;RTA_OK(rtap, rtl);rtap=RTA_NEXT(rtap,rtl)){
+      switch(rtap->rta_type){
         // destination IPv4 address
         case RTA_DST:
-          inet_ntop(AF_INET, RTA_DATA(rtap),
-                                     dsts, 24);
+          inet_ntop(AF_INET, RTA_DATA(rtap), dsts, 24);
           break;
-
         // next hop IPv4 address
         case RTA_GATEWAY:
-          inet_ntop(AF_INET, RTA_DATA(rtap),
-                                     gws, 24);
+          inet_ntop(AF_INET, RTA_DATA(rtap), gws, 24);
           break;
-
         // unique ID associated with the network
         // interface
         case RTA_OIF:
-          sprintf(ifs, "%d",
-                   *((int *) RTA_DATA(rtap)));
+          sprintf(ifs, "%d", *((int *) RTA_DATA(rtap)));
+          break;
         default:
+          // assert(false);
           break;
       }
     }
     sprintf(ms, "%d", rtp->rtm_dst_len);
-
-    printf("dst %s/%s gw %s if %s\n",
-                          dsts, ms, gws, ifs);
+    printf("dst %s/%s gw %s if %s\n", dsts, ms, gws, ifs);
   }
+  // close socket
+  close(fd);
+
+  fd=-1;
+
 }
+
 
 
 
