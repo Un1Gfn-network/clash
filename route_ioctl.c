746e2e8bf8223aa05dabf7e8eafa0d9a586aa659
Darren Ng <darren19970810@gmail.com>
2020-09-15 14:43:49 +0800

    AF_PACKET sockaddr_ll sll_addr sll_halen sll_hatype

--- route_ioctl.c
+++ route_ioctl.c
@@ -1,20 +1,21 @@
 #include <assert.h>
 #include <errno.h>
-#include <ifaddrs.h> // getifaddrs()
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
-#include <linux/sockios.h> // provides SIOCETHTOOL
-#include <sys/ioctl.h> // no SIOCETHTOOL in <bits/ioctls.h>
-
 #include <unistd.h>
 
 #include <arpa/inet.h> // inet_ntop
+#include <bsd/stdlib.h> // humanize_number(3bsd)
+#include <ifaddrs.h> // getifaddrs()
 #include <linux/if_link.h> // struct rtnl_link_stats
+#include <linux/if_packet.h> // struct sockaddr_ll
 #include <net/if_arp.h> // ARPHRD_* device type
 #include <net/route.h> // struct rtentry
+
+#include <linux/sockios.h> // provides SIOCETHTOOL
+#include <sys/ioctl.h> // no SIOCETHTOOL in <bits/ioctls.h>
 
 // <net/if.h> <linux/if.h>
 //     X            O      IFF_LOWER_UP
@@ -22,9 +23,8 @@
 #define IFF_LOWER_UP (1<<16)
 #include <net/if.h>
 
-#include <bsd/stdlib.h> // humanize_number(3bsd)
-
-#include <linux/ethtool.h>
+// struct ethtool_cmd(deprecated) and ethtool_link_settings
+// #include <linux/ethtool.h>
 
 #include "def.h"
 
@@ -141,27 +141,40 @@ void steal_flag_u32(unsigned *flags,const unsigned f,const char *const s){
 void addr(const char *t,struct sockaddr *addr){
 
   assert(addr);
-  if(addr->sa_family==AF_PACKET){
-    printf("%s (pkt) ",t);
-    return;
-  }
-
-  // printf("[%d] \n",sin->sin_family);
-  struct sockaddr_in *sin=(struct sockaddr_in*)(addr);
-  assert(sin->sin_port==0);
-
   (t&&strlen(t))?printf("%s ",t):0;
 
-  if(sin->sin_family==AF_INET6){
-    char s[INET6_ADDRSTRLEN]={};
-    assert(s==inet_ntop(AF_INET6,&(sin->sin_addr),s,INET6_ADDRSTRLEN));
-    printf("[%s] ",s);
-  }else if(sin->sin_family==AF_INET){
-    char s[INET_ADDRSTRLEN]={};
-    assert(s==inet_ntop(AF_INET,&(sin->sin_addr),s,INET_ADDRSTRLEN));
-    printf("%s ",s);
+  if(addr->sa_family==AF_PACKET){
+
+    // packet(7)
+    const struct sockaddr_ll *const s=(struct sockaddr_ll*)addr;
+    assert(
+      s->sll_family==AF_PACKET &&
+      s->sll_protocol==0x0 &&
+      // sll_ifindex
+      // sll_hatype
+      s->sll_pkttype==PACKET_HOST &&
+      s->sll_halen==6
+      // sll_addr[8]
+    );
+    for(int j=0;j<(s->sll_halen);++j)
+      printf("%02X%s",s->sll_addr[j],j<(s->sll_halen-1)?":":" ");
+
   }else{
-    assert(false);
+    // printf("[%d] \n",sin->sin_family);
+    struct sockaddr_in *sin=(struct sockaddr_in*)(addr);
+    assert(sin->sin_port==0);
+
+    if(sin->sin_family==AF_INET6){
+      char s[INET6_ADDRSTRLEN]={};
+      assert(s==inet_ntop(AF_INET6,&(sin->sin_addr),s,INET6_ADDRSTRLEN));
+      printf("[%s] ",s);
+    }else if(sin->sin_family==AF_INET){
+      char s[INET_ADDRSTRLEN]={};
+      assert(s==inet_ntop(AF_INET,&(sin->sin_addr),s,INET_ADDRSTRLEN));
+      printf("%s ",s);
+    }else{
+      assert(false);
+    }
   }
 
 }
@@ -190,6 +203,17 @@ void netdevice(const char *const name){
   #define REFILL() {ifr=(struct ifreq){};strncpy(ifr.ifr_name,name,IFNAMSIZ);r=0;errno=0;}
 
   // SIOCGIFNAME and SIOCGIFINDEX in onebyone()
+
+  REFILL();
+  assert(0==ioctl(sockfd,SIOCGIFHWADDR,&ifr));
+  switch(ifr.ifr_hwaddr.sa_family){
+    case ARPHRD_LOOPBACK:printf("loopback ");break;
+    case ARPHRD_ETHER:printf("ethernet ");break;
+    case ARPHRD_NONE:printf("noheader ");break;
+    default: assert(false);
+  }
+  const unsigned char *const h=(unsigned char*)ifr.ifr_hwaddr.sa_data;
+  printf("hwaddr %02x:%02x:%02x:%02x:%02x:%02x ",h[0],h[1],h[2],h[3],h[4],h[5]);
 
   REFILL();
   assert(0==ioctl(sockfd,SIOCGIFFLAGS,&ifr));
@@ -221,16 +245,6 @@ void netdevice(const char *const name){
   REFILL();
   assert(0==ioctl(sockfd,SIOCGIFMTU,&ifr));
   printf("mtu %d ",ifr.ifr_mtu);
-
-  REFILL();
-  assert(0==ioctl(sockfd,SIOCGIFHWADDR,&ifr));
-  switch(ifr.ifr_hwaddr.sa_family){
-    case ARPHRD_LOOPBACK:printf("loopback ");break;
-    case ARPHRD_ETHER:printf("ethernet ");break;
-    case ARPHRD_NONE:printf("noheader ");break;
-  }
-  const unsigned char *const h=(unsigned char*)ifr.ifr_hwaddr.sa_data;
-  printf("hwaddr %02x:%02x:%02x:%02x:%02x:%02x ",h[0],h[1],h[2],h[3],h[4],h[5]);
 
   REFILL();
   assert(0==ioctl(sockfd,SIOCGIFMAP,&ifr));
@@ -314,7 +328,7 @@ void all_ifconf(){
     (char*)ifc.ifc_req==buf
   );
   for(int i=0;i<n;++i){
-    printf("#%d ",ifc.ifc_req[i].ifr_ifindex);
+    // printf("#%d ",ifc.ifc_req[i].ifr_ifindex);
     printf(NAME_FMT,ifc.ifc_req[i].ifr_name);
     addr(NULL,&(ifc.ifc_req[i].ifr_addr));
     printf("\n");
@@ -327,7 +341,30 @@ void all_getifaddrs(){
   assert(ifa);
 
   for(struct ifaddrs *i=ifa;i;i=i->ifa_next){
+
+    #define PKT(x) ((const struct sockaddr_ll*)x)
+    const bool pkt=(i->ifa_addr&&i->ifa_addr->sa_family==AF_PACKET);
+    pkt?assert(
+      !(i->ifa_netmask) &&
+      i->ifa_broadaddr &&
+      i->ifa_dstaddr &&
+      i->ifa_broadaddr->sa_family==AF_PACKET &&
+      i->ifa_dstaddr->sa_family==AF_PACKET /*&&
+      PKT(i->ifa_addr)->sll_ifindex==PKT(i->ifa_broadaddr)->sll_ifindex &&
+      PKT(i->ifa_addr)->sll_ifindex==PKT(i->ifa_dstaddr)->sll_ifindex*/
+    ):0;
+
+    // pkt?printf("#%d ",PKT(i)->sll_ifindex):0;
     printf(NAME_FMT,i->ifa_name);
+    if(pkt){
+      switch(PKT(i->ifa_addr)->sll_hatype){
+        case ARPHRD_LOOPBACK:printf("loopback ");break;
+        case ARPHRD_ETHER:printf("ethernet ");break;
+        case ARPHRD_NONE:printf("noheader ");break;
+        default:assert(false);
+      };
+      printf("| ");
+    }
     const unsigned flags=i->ifa_flags;
     steal_flag_u32(&(i->ifa_flags),IFF_UP,"up");
     steal_flag_u32(&(i->ifa_flags),IFF_BROADCAST,"broadcast");
@@ -338,26 +375,27 @@ void all_getifaddrs(){
     steal_flag_u32(&(i->ifa_flags),IFF_MULTICAST,"multicast");
     steal_flag_u32(&(i->ifa_flags),IFF_LOWER_UP,"l1up");
     assert(i->ifa_flags==0);
-
-    if(i->ifa_addr&&i->ifa_addr->sa_family==AF_PACKET){
-      assert(
-        !(i->ifa_netmask) &&
-        i->ifa_broadaddr->sa_family==AF_PACKET &&
-        i->ifa_dstaddr->sa_family==AF_PACKET
-      );
+    if(pkt){
       struct rtnl_link_stats *stats=i->ifa_data;
       assert(stats);
       printf("tx %u ",stats->tx_packets);
       humanize(stats->tx_bytes);
       printf("rx %u ",stats->rx_packets);
       humanize(stats->rx_bytes);
-    }else{
-      i->ifa_addr?addr(NULL,i->ifa_addr):0;
-      i->ifa_netmask?addr("mask",i->ifa_netmask):0;
-      assert(!(flags&IFF_BROADCAST&IFF_POINTOPOINT)); // Mutually exclusive
-      i->ifa_broadaddr?addr("bcast",i->ifa_broadaddr):0;
-      i->ifa_dstaddr?addr("dst",i->ifa_dstaddr):0;
     }
+    printf("\n");
+
+    assert(!(flags&IFF_BROADCAST&IFF_POINTOPOINT)); // Mutually exclusive
+    i->ifa_addr?addr(NULL,i->ifa_addr):0;
+    i->ifa_netmask?addr("mask",i->ifa_netmask):0;
+    i->ifa_broadaddr?addr("bcast",i->ifa_broadaddr):0;
+    i->ifa_dstaddr?addr("dst",i->ifa_dstaddr):0;
+    (
+      i->ifa_addr ||
+      i->ifa_netmask ||
+      i->ifa_broadaddr ||
+      i->ifa_dstaddr
+    )?printf("\n"):0;
 
     printf("\n");
 
@@ -369,16 +407,18 @@ void all_getifaddrs(){
 int main(){
 
   init();
+  printf("\n");
 
   // set();
   // getchar();
   // reset();
 
-  printf("\n");
   onebyone();
   printf("\n");
+
   all_ifconf();
   printf("\n");
+
   all_getifaddrs();
   printf("\n");
 
